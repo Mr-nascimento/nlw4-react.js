79cce8616013806b657081d1ddb243f41063f3a8
Mr-nascimento <igortjnascimento@gmail.com>
2021-03-16 22:51:10 -0300

    Primeira versão do projeto

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/mr-nascimento.png" alt="Igor Nascimento" />
+            <div>
+                <strong>Igor Nascimento</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    )
+}
